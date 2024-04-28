ca14843811cf9356cec17144df59bf92959dfdce
てんでん <barururu3.pororo.kmt@gmail.com>
2024-04-29 04:46:17 +0900

    ヘッダーのリンクを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,14 +12,17 @@ import "./globals.css";
 
 export const metadata = {
   title: "Farcaster Account Checker",
-  description: "Check Farcaster account information.",
+  description:
+    "You can check how many holders of a given NFT collection have registered an account with Farcaster.",
 };
 
 const Header = () => (
   <>
     <Center>
       <Stack pt={20}>
-        <Title>{metadata.title}</Title>
+        <a href="/">
+          <Title>{metadata.title}</Title>
+        </a>
       </Stack>
     </Center>
   </>
